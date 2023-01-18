81713c1663f259d4b03a9d1c34387dfd8638181b
Sakti Yasin <saktie.banua@gmail.com>
2023-01-18 21:05:11 +0700

    Update

--- resources/js/components/front/Main.vue
+++ resources/js/components/front/Main.vue
@@ -9,7 +9,7 @@
       <ul>
         <li v-if="!$store.state.sideMenu" @click="showLoginModal">
           <i class="bx bx-log-in-circle"></i>
-          <span class="tooltip-sidebar">Logins</span>
+          <span class="tooltip-sidebar">Login</span>
         </li>
         <li v-if="!$store.state.sideMenu" @click="showRegisterModal">
           <i class="bx bx-user-plus"></i>
@@ -82,7 +82,7 @@
                         <span class="icon">
                           <i class="bx bx-log-in-circle"></i>
                         </span>
-                        <span class="title">Login</span>
+                        <span class="title">Logins</span>
                       </span>
                       <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg">
                         <path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path>
